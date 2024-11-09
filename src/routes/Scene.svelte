11f7e9512be829d4d6b63bbb12df404f3c4d35f4
jollygrin <JollyGrin@protonmail.com>
2024-11-09 18:03:07 +0100

    fix line

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -11,12 +11,12 @@
 
   const locations = [
     { lat: 52.3676, lon: 4.9041 }, // Amsterdam
+    { lat: 35.6762, lon: 139.6503 }, // Tokyo
     { lat: 30.2672, lon: -97.7431 }, // Austin
-    { lat: 40.7128, lon: -74.006 }, // New York
     { lat: 51.5074, lon: -0.1278 }, // London
+    { lat: 40.7128, lon: -74.006 }, // New York
     { lat: 48.8566, lon: 2.3522 }, // Paris
     { lat: 34.0522, lon: -118.2437 }, // Los Angeles
-    { lat: 35.6762, lon: 139.6503 }, // Tokyo
     { lat: 39.9042, lon: 116.4074 }, // Beijing
     { lat: -33.8688, lon: 151.2093 }, // Sydney
     { lat: 40.7306, lon: -73.9352 }, // Brooklyn
@@ -36,12 +36,26 @@
 
   const start = latLongToVector3(locations[0].lat, locations[0].lon, 5);
   const end = latLongToVector3(locations[1].lat, locations[1].lon, 5);
-  const controlPoint = new Vector3(
-    (start.x + end.x) / 2,
-    (start.y + end.y) / 2 + 2.5, // Add some height to make the arc curve
-    (start.z + end.z) / 2,
-  );
-  const curve = new CatmullRomCurve3([start, controlPoint, end]);
+  // const controlPoint = new Vector3(
+  //   (start.x + end.x) / 2,
+  //   (start.y + end.y) / 2 + 2.5, // Add some height to make the arc curve
+  //   (start.z + end.z) / 2,
+  // );
+
+  const controlPoints = [
+    new Vector3(
+      start.x + (end.x - start.x) * 0.25, // Closer to start
+      start.y + Math.max((end.y - start.y) * 1.25, 2), // Slightly elevated
+      start.z + (end.z - start.z) * 0.25,
+    ),
+    new Vector3(
+      end.x - (end.x - start.x) * 0.25, // Closer to end
+      end.y + Math.max((end.y - start.y) * 1.25, 3), // Elevated similarly to create symmetry
+      end.z - (end.z - start.z) * 0.25,
+    ),
+  ];
+
+  const curve = new CatmullRomCurve3([start, ...controlPoints, end]);
   curve.closed = false;
 
   let pos: { x: number; y: number; z: number } = { x: 0, y: 0, z: 0 }; // Initialize mesh variable
