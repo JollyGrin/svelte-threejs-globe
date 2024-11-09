b079a61e0163e29e824adfe0777a890ebf1594ef
jollygrin <JollyGrin@protonmail.com>
2024-11-09 17:03:49 +0100

    make curve work

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -3,12 +3,93 @@
   import Lights from "./Lights.svelte";
   import Earth from "./Earth.svelte";
   import Dot from "./Dot.svelte";
+  import { latLongToVector3 } from "$lib/helpers/latlong";
+  import {
+    CatmullRomCurve3,
+    MeshBasicMaterial,
+    TubeGeometry,
+    Vector3,
+  } from "three";
+  import { T } from "@threlte/core";
+  import { onMount } from "svelte";
   let globeRadius: number = 5;
 
   const locations = [
     { lat: 40.7128, lon: -74.006 }, // New York
     { lat: 51.5074, lon: -0.1278 }, // London
+    { lat: 48.8566, lon: 2.3522 }, // Paris
+    { lat: 34.0522, lon: -118.2437 }, // Los Angeles
+    { lat: 35.6762, lon: 139.6503 }, // Tokyo
+    { lat: 39.9042, lon: 116.4074 }, // Beijing
+    { lat: -33.8688, lon: 151.2093 }, // Sydney
+    { lat: 40.7306, lon: -73.9352 }, // Brooklyn
+    { lat: 55.7558, lon: 37.6173 }, // Moscow
+    { lat: 48.2082, lon: 16.3738 }, // Vienna
+    { lat: 41.9028, lon: 12.4964 }, // Rome
+    { lat: 52.52, lon: 13.405 }, // Berlin
+    { lat: -34.6037, lon: -58.3816 }, // Buenos Aires
+    { lat: 22.3964, lon: 114.1095 }, // Hong Kong
+    { lat: 40.7306, lon: -73.9352 }, // Chicago
+    { lat: 19.4326, lon: -99.1332 }, // Mexico City
+    { lat: 37.7749, lon: -122.4194 }, // San Francisco
+    { lat: 43.6532, lon: -79.3832 }, // Toronto
+    { lat: 34.0522, lon: -118.2437 }, // Los Angeles
+    { lat: 37.9838, lon: 23.7275 }, // Athens
   ];
+
+  // Create arc geometry (Path between two locations)
+  function createArc(
+    from: { lat: number; lon: number },
+    to: { lat: number; lon: number },
+    radius: number,
+  ) {
+    const start = latLongToVector3(from.lat, from.lon, radius);
+    const end = latLongToVector3(to.lat, to.lon, radius);
+
+    // Create a curve path between two points
+    const controlPoint = new Vector3(
+      (start.x + end.x) / 2,
+      (start.y + end.y) / 2 + 1, // Add some height to make the arc curve
+      (start.z + end.z) / 2,
+    );
+
+    // Use a CatmullRomCurve to create a smooth arc
+    const curve = new CatmullRomCurve3([start, controlPoint, end]);
+
+    // Make sure the curve is closed and has the necessary methods
+    curve.closed = false;
+
+    // Create TubeGeometry along the curve path
+    const tubeGeometry = new TubeGeometry(curve, 64, 0.2, 8, false);
+    const material = new MeshBasicMaterial({ color: "white" });
+
+    // Return geometry and material for use in Threlte Mesh
+    return { tubeGeometry, material };
+  }
+
+  // Create arcs between the first location and the other locations
+  let arcs: { tubeGeometry: TubeGeometry; material: MeshBasicMaterial }[] = [];
+
+  onMount(() => {
+    arcs = [
+      createArc(locations[0], locations[1], globeRadius),
+      createArc(locations[0], locations[2], globeRadius),
+    ];
+    // arcs.push(createArc(locations[0], locations[1], globeRadius));
+    // arcs.push(createArc(locations[0], locations[2], globeRadius));
+
+    console.log({ arcs });
+  });
+
+  const start = latLongToVector3(locations[0].lat, locations[0].lon, 5);
+  const end = latLongToVector3(locations[1].lat, locations[1].lon, 5);
+  const controlPoint = new Vector3(
+    (start.x + end.x) / 2,
+    (start.y + end.y) / 2 + 1, // Add some height to make the arc curve
+    (start.z + end.z) / 2,
+  );
+  const curve = new CatmullRomCurve3([start, controlPoint, end]);
+  curve.closed = false;
 </script>
 
 <Lights />
@@ -18,3 +99,8 @@
 {#each locations as location}
   <Dot {location} />
 {/each}
+
+<T.Mesh>
+  <T.TubeGeometry args={[curve, 124, 0.01]} />
+  <T.MeshBasicMaterial color="white" />
+</T.Mesh>
