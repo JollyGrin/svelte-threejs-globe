3f1ae27d8140d59b4b52fc46861d5dd5296b0adb
jollygrin <JollyGrin@protonmail.com>
2024-12-04 16:51:49 +0100

    remove github pages link

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,6 @@
 export const prerender = true;
 
 export function load() {
-	const basePath =
-		import.meta.env.MODE === 'production' ? '/svelte-threejs-globe' : '';
-	return { basePath };
+  const basePath = import.meta.env.MODE === "production" ? "" : "";
+  return { basePath };
 }
